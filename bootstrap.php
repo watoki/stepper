59d8de0eaa9472895ce97943ccfc3469d1bb75ba
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-13 14:56:31 +0200

    Fixed bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,21 +2,22 @@
 
 $dir = __DIR__;
 
-$files = array();
 do {
-    $files[] = $dir . '/autoload.php';
-    $files[] = $dir . '/vendor/autoload.php';
+    $files = array(
+        $dir . '/autoload.php' ,
+        $dir . '/vendor/autoload.php'
+    );
+
+    foreach ($files as $file) {
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
 
     $lastDir = $dir;
     $dir = realpath($dir . '/..');
 } while (substr($lastDir, -6) != 'vendor');
-
-foreach ($files as $file) {
-    if (file_exists($file)) {
-        require_once $file;
-        return;
-    }
-}
 
 echo "ERROR: Could not find auto loader in: " . json_encode($files);
 exit(1);
