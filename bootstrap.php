df8016f18847505afc5c3c3a19b1648c1dd2dc32
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-13 14:44:31 +0200

    Prepared bootstrap.php for use as vendor library

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+$dir = __DIR__;
+
+$files = array();
+do {
+    $files[] = $dir . '/autoload.php';
+    $files[] = $dir . '/vendor/autoload.php';
+
+    $lastDir = $dir;
+    $dir = realpath($dir . '/..');
+} while (substr($lastDir, -6) != 'vendor');
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+        return;
+    }
+}
+
+echo "ERROR: Could not find auto loader in: " . json_encode($files);
+exit(1);
