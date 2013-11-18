8375ca9aad1b4d15a6f89720809d6670b69e00ee
Nikolas Martens <Nikolas.M@rtens.org>
2013-11-18 14:40:23 +0100

    Replaced unnecessarily complex bootstrap script

--- bootstrap.php
+++ bootstrap.php
@@ -1,23 +1,3 @@
 <?php
 
-$dir = __DIR__;
-
-do {
-    $files = array(
-        $dir . '/autoload.php' ,
-        $dir . '/vendor/autoload.php'
-    );
-
-    foreach ($files as $file) {
-        if (file_exists($file)) {
-            require_once $file;
-            return;
-        }
-    }
-
-    $lastDir = $dir;
-    $dir = realpath($dir . '/..');
-} while (substr($lastDir, -6) != 'vendor');
-
-echo "ERROR: Could not find auto loader in: " . json_encode($files);
-exit(1);
+require __DIR__ . '/vendor/autoload.php';
